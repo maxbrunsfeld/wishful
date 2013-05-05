236820cbad49e7fda74157fa0ee0304cbf3adfe8
Max Brunsfeld <maxbrunsfeld@gmail.com>
2013-05-04 20:23:55 -0700

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject wishful "0.1.0-SNAPSHOT"
+(defproject wishful "0.1.1"
   :description "A small stubbing and spying library"
-  :url "http://github.com/maxbrunsfeld/wishful"
+  :url "https://github.com/maxbrunsfeld/wishful"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :scm {:name "git"
+        :url "https://github.com/maxbrunsfeld/wishful"})
